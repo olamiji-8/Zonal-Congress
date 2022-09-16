f32103129fb7dd01b855179d873b0616004f8dc5
olamiji-8 <olamijisotomi@gmail.com>
2022-09-16 09:17:13 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home/Home';
+import Sermon from './Sermon/Sermon';
+import Livestream from './Livestream/Livestream';
 
 function App() {
   return (
     <div>
-     
+     <Home/>
+     <Sermon/>
+     <Livestream/>
     </div>
   );
 }

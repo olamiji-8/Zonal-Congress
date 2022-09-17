2dffa4523434410cb7c50d9a6eafd4d369178129
olamiji-8 <olamijisotomi@gmail.com>
2022-09-17 10:48:49 +0100

    update

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import Navbar from '../Component/Navbar/Navbar'
 import Zonal from "../Assets/zonal.jpeg";
 
+
 const Home = () => {
   return (
     <div>
+      
         <Navbar/>
        <img src={Zonal} alt='' width="100%" className='zonal'/>
     </div>

75d251dea943ebc51e3b1ec994751c8b54046378
olamiji-8 <olamijisotomi@gmail.com>
2022-09-17 17:50:35 +0100

    update

--- src/Livestream/Livestream.jsx
+++ src/Livestream/Livestream.jsx
@@ -7,7 +7,7 @@ const Livestream = () => {
     <div id='livestream'>
         <h1 className='heading'>Live Stream</h1>
                 
-        <h3 className='sub-head'>To join us in the service, you can click on the radio button. </h3>
+        <h3 className='sub-head'>Join the service by clicking the button. </h3>
         <div className='radio'>  
         <a href='https://bit.ly/3xs8KWa'><img src={Live} alt='' width="80px"/></a>
         </div>
